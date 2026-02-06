00716c8ffcc7135da2d43d246f40168d5bd8f0c8
Franco Galluccio <francoivangalluccio@gmail.com>
2026-02-06 15:03:49 -0300

    SIMPLIFIED accounts drawer

--- src/shared/layouts/components/account-drawer.tsx
+++ src/shared/layouts/components/account-drawer.tsx
@@ -2,22 +2,9 @@
 
 import type { BoxProps } from '@mui/material/Box';
 
-import { useState, useCallback } from 'react';
-import { useUser, useClerk, UserButton } from '@clerk/nextjs';
+import { UserButton } from '@clerk/nextjs';
 
 import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Drawer from '@mui/material/Drawer';
-import { useTheme } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-
-import { varAlpha } from 'src/shared/theme/styles';
-import { Iconify } from 'src/shared/components/minimal/iconify';
-import { Scrollbar } from 'src/shared/components/minimal/scrollbar';
-import { AnimateAvatar } from 'src/shared/components/minimal/animate';
-
-import { SignOutButton } from './sign-out-button';
 
 // ----------------------------------------------------------------------
 
@@ -31,157 +18,11 @@ export type AccountDrawerProps = BoxProps & {
 };
 
 export function AccountDrawer({ data = [], sx, ...other }: AccountDrawerProps) {
-  const theme = useTheme();
-
-  const { user } = useUser();
-
-  const { signOut } = useClerk();
-
-  const [open, setOpen] = useState(false);
-
-  const handleCloseDrawer = useCallback(() => {
-    setOpen(false);
-  }, []);
-
-  const handleSignOut = async () => {
-    try {
-      await signOut();
-      handleCloseDrawer();
-    } catch (error) {
-      console.error('Error al cerrar sesión:', error);
-    }
-  };
-
-  const renderAvatar = (
-    <AnimateAvatar
-      width={96}
-      slotProps={{
-        avatar: { src: '', alt: user?.username ?? '' },
-        overlay: {
-          border: 2,
-          spacing: 3,
-          color: `linear-gradient(135deg, ${varAlpha(theme.vars.palette.primary.mainChannel, 0)} 25%, ${theme.vars.palette.primary.main} 100%)`,
-        },
-      }}
-    >
-      {user?.username?.charAt(0).toUpperCase()}
-    </AnimateAvatar>
-  );
-
   return (
     <Box sx={{ display: 'inline-flex', ...sx }} {...other}>
-      {/* <AccountButton
-        open={open}
-        onClick={handleOpenDrawer}
-        photoURL=""
-        displayName={user?.username ?? ''}
-        sx={sx}
-        {...other}
-      /> */}
-      <UserButton />
-
-      <Drawer
-        open={open}
-        onClose={handleCloseDrawer}
-        anchor="right"
-        slotProps={{ backdrop: { invisible: true } }}
-        PaperProps={{ sx: { width: 320 } }}
-      >
-        <IconButton
-          onClick={handleCloseDrawer}
-          sx={{ top: 12, left: 12, zIndex: 9, position: 'absolute' }}
-        >
-          <Iconify icon="mingcute:close-line" />
-        </IconButton>
-
-        <Scrollbar>
-          <Stack alignItems="center" sx={{ pt: 8 }}>
-            {renderAvatar}
-
-            <Typography variant="subtitle1" noWrap sx={{ mt: 2 }}>
-              {user?.username ?? ''}
-            </Typography>
-
-            <Typography variant="body2" sx={{ color: 'text.secondary', mt: 0.5 }} noWrap>
-              {user?.primaryEmailAddress?.emailAddress ?? ''}
-            </Typography>
-          </Stack>
-
-          {/* <Stack direction="row" spacing={1} flexWrap="wrap" justifyContent="center" sx={{ p: 3 }}>
-            {[...Array(3)].map((_, index) => (
-              <Tooltip
-                key={_mock.fullName(index + 1)}
-                title={`Switch to: ${_mock.fullName(index + 1)}`}
-              >
-                <Avatar
-                  alt={_mock.fullName(index + 1)}
-                  src={_mock.image.avatar(index + 1)}
-                  onClick={() => {}}
-                />
-              </Tooltip>
-            ))}
-
-            <Tooltip title="Add account">
-              <IconButton
-                sx={{
-                  bgcolor: varAlpha(theme.vars.palette.grey['500Channel'], 0.08),
-                  border: `dashed 1px ${varAlpha(theme.vars.palette.grey['500Channel'], 0.32)}`,
-                }}
-              >
-                <Iconify icon="mingcute:add-line" />
-              </IconButton>
-            </Tooltip>
-          </Stack> */}
-
-          {/* <Stack
-            sx={{
-              py: 3,
-              px: 2.5,
-              borderTop: `dashed 1px ${theme.vars.palette.divider}`,
-              borderBottom: `dashed 1px ${theme.vars.palette.divider}`,
-            }}
-          >
-            {data.map((option) => {
-              const rootLabel = pathname.includes('/dashboard') ? 'Home' : 'Dashboard';
-
-              const rootHref = pathname.includes('/dashboard') ? '/' : paths.dashboard.root;
-
-              return (
-                <MenuItem
-                  key={option.label}
-                  onClick={() => handleClickItem(option.label === 'Home' ? rootHref : option.href)}
-                  sx={{
-                    py: 1,
-                    color: 'text.secondary',
-                    '& svg': { width: 24, height: 24 },
-                    '&:hover': { color: 'text.primary' },
-                  }}
-                >
-                  {option.icon}
-
-                  <Box component="span" sx={{ ml: 2 }}>
-                    {option.label === 'Home' ? rootLabel : option.label}
-                  </Box>
-
-                  {option.info && (
-                    <Label color="error" sx={{ ml: 1 }}>
-                      {option.info}
-                    </Label>
-                  )}
-                </MenuItem>
-              );
-            })}
-          </Stack> */}
-
-          {/* <Box sx={{ px: 2.5, py: 3 }}>
-            <UpgradeBlock />
-          </Box> */}
-        </Scrollbar>
-
-        <Box sx={{ p: 2.5 }}>
-          <SignOutButton onClose={handleSignOut} />
-        </Box>
-      </Drawer>
+      <Box sx={{ position: 'relative' }}>
+        <UserButton showName />
+      </Box>
     </Box>
   );
 }
